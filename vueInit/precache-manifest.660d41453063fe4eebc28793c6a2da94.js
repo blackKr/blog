010e178ebea2a752bef42034c12d909bfbf3577a
wjc201314 <1756934743@qq.com>
2020-01-27 00:29:51 +0800

    增加vue初始化的打包文件

---
+++ vueInit/precache-manifest.660d41453063fe4eebc28793c6a2da94.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9580dc0fcefcd11adb5f77e4730bcb14",
+    "url": "index.html"
+  },
+  {
+    "revision": "db5cfcae7210548744ee1b67e2259273",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "85145dc9d1877c430b1f",
+    "url": "static/css/app.fa28f441.css"
+  },
+  {
+    "revision": "2c2537b8f8485296c1b4",
+    "url": "static/css/chunk-28a17c58.8609a47c.css"
+  },
+  {
+    "revision": "9eb1ba33ce431b9fb9d5",
+    "url": "static/css/chunk-6cb83182.7c895eff.css"
+  },
+  {
+    "revision": "31a600f741f399b47103",
+    "url": "static/css/chunk-6cc52828.940c0878.css"
+  },
+  {
+    "revision": "729711ef4af286e55aee",
+    "url": "static/css/chunk-vendors.717c90ab.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "static/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "static/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "static/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "85145dc9d1877c430b1f",
+    "url": "static/js/app.73c68b2a.js"
+  },
+  {
+    "revision": "2c2537b8f8485296c1b4",
+    "url": "static/js/chunk-28a17c58.7d0f2b5f.js"
+  },
+  {
+    "revision": "ec9ede6b187554d80477",
+    "url": "static/js/chunk-2d0e8c14.b7fd95b2.js"
+  },
+  {
+    "revision": "30c0ba1e9092fc7712e7",
+    "url": "static/js/chunk-2d0f1194.c7a07ccb.js"
+  },
+  {
+    "revision": "9eb1ba33ce431b9fb9d5",
+    "url": "static/js/chunk-6cb83182.077bd975.js"
+  },
+  {
+    "revision": "31a600f741f399b47103",
+    "url": "static/js/chunk-6cc52828.4977fff8.js"
+  },
+  {
+    "revision": "729711ef4af286e55aee",
+    "url": "static/js/chunk-vendors.ef171db8.js"
+  }
+]);
